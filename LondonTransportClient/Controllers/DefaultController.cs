12d356f5630115591bffbaabf0f6a4601783d5d8
Александр <imperaxel@gmail.com>
2017-03-15 02:31:11 +0300

    Add LondonTransportApiClient.

---
+++ LondonTransportClient/Controllers/DefaultController.cs
@@ -0,0 +1,87 @@
+﻿using System.Web.Mvc;
+using LondonTransportClient.RestApi;
+using Tfl.Api.Presentation.Entities;
+
+namespace LondonTransportClient.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly LondonTransportApi _londonTransportApi = new LondonTransportApi();
+
+        public ActionResult RoadDisruption()
+        {
+            var response = _londonTransportApi.RoadDisruption();
+            return View(response);
+        }
+
+        public ActionResult ListOfArrivalPredictionId(string searchPredicationId)
+        {
+            if (string.IsNullOrEmpty(searchPredicationId) || string.IsNullOrWhiteSpace(searchPredicationId))
+            {
+                return View("ListOfArrivalPredictionId");
+            }
+            var response = _londonTransportApi.ListOfArrivalPrediction(searchPredicationId);
+            if (response is ApiError)
+            {
+                return View("ListOfArrivalPredictionError");
+            }
+            return View(response);
+        }
+
+        public ActionResult GetVehicleById(string vehicleId)
+        {
+            if (string.IsNullOrEmpty(vehicleId) || string.IsNullOrWhiteSpace(vehicleId))
+            {
+                return View("GetVehicleById");
+            }
+            var response = _londonTransportApi.GetVehicleById(vehicleId);
+            if (response.Count == 0)
+            {
+                return View("GetVehicleByIdError");
+            }
+            return View(response);
+        }
+
+        public ActionResult SearchBikeStantionByNane(string searchName)
+        {
+            if (string.IsNullOrEmpty(searchName) || string.IsNullOrWhiteSpace(searchName))
+            {
+                return View("SearchBikeStantionByNane");
+            }
+            var response = _londonTransportApi.SearchBikeStantionByNane(searchName);
+            if (response.Count == 0)
+            {
+                return View("SearchBikeStantionByNaneError");
+            }
+            return View(response);
+        }
+
+        public ActionResult StationSearch(string term)
+        {
+            var response = _londonTransportApi.StationSearch(term);
+            return View(response);
+        }
+
+        public ActionResult RoadState()
+        {
+            var response = _londonTransportApi.RoadState();
+            return View(response);
+        }
+
+        public ActionResult AirQuality()
+        {
+            var response = _londonTransportApi.AirQuality();
+            return View(response);
+        }
+
+        public ActionResult GetOccupacityCarParkId(string searchId)
+        {
+            if (searchId == null)
+            {
+                return View("GetOccupacityCarParkId");
+            }
+            var response = _londonTransportApi.GetOccupacityCarParkId(searchId);
+            return View(response);
+        }
+    }
+}
---
+++ LondonTransportClient/RestApi/LondonTransportApi.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+using Tfl.Api.Presentation.Entities;
+
+namespace LondonTransportClient.RestApi
+{
+    public class LondonTransportApi
+    {
+        private const string AppId = "dd94f504";
+        private const string AppKey = "0655bb9d0faa12ecf660743871882083";
+        private const string ApiRootUrl = @"https://api-argon.tfl.gov.uk";
+        private const string OldApiRootUrl = @"https://api.tfl.gov.uk";
+
+        public RouteSearchResponse StationSearch(string term)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{ApiRootUrl}/Line/Search/{term}?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<RouteSearchResponse>(content);
+            return deserialised;
+        }
+
+        public List<RoadCorridor> RoadState()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{ApiRootUrl}/Road/?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<List<RoadCorridor>>(content);
+            return deserialised;
+        }
+
+        public LondonAirForecast AirQuality()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{ApiRootUrl}/AirQuality?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<LondonAirForecast>(content);
+            return deserialised;
+        }
+
+        public CarParkOccupancy GetOccupacityCarParkId(string term)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{ApiRootUrl}/Occupancy/CarPark/{term}?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<CarParkOccupancy>(content);
+            return deserialised;
+        }
+
+        public List<StatusSeverity> RoadDisruption()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{ApiRootUrl}/Road/Meta/Severities?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<List<StatusSeverity>>(content);
+            return deserialised;
+        }
+
+        public object ListOfArrivalPrediction(string term)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{ApiRootUrl}/StopPoint/{term}/Arrivals?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                var contentError = response.Content;
+                var deserialisedError = JsonConvert.DeserializeObject<ApiError>(contentError);
+                return deserialisedError;
+            }
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<List<Prediction>>(content);
+            return deserialised;
+        }
+
+        public List<Prediction> GetVehicleById(string term)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{OldApiRootUrl}/Vehicle/{term}/Arrivals?app_id={AppId}&app_key={AppKey}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<List<Prediction>>(content);
+            return deserialised;
+        }
+
+        public List<Place> SearchBikeStantionByNane(string term)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient($"{OldApiRootUrl}/BikePoint/Search?query={term}&app_key={AppKey}&app_id={AppId}");
+            var request = new RestRequest("/", Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            var response = (RestResponse)client.Execute(request);
+            var content = response.Content;
+            var deserialised = JsonConvert.DeserializeObject<List<Place>>(content);
+            return deserialised;
+        }
+    }
+}
